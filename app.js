3b9318a7e21be40f7301ff21f2988251b18df379
Mitch Ball <mitchb145@gmail.com>
2017-07-05 15:33:18 +1000

    Add getStories, /top now returns story objects instead of story IDs

--- app.js
+++ app.js
@@ -11,13 +11,31 @@ app.get('/', (req, res) => (res.send('Hello World')))
 
 // Get IDs of top 10 stories
 app.get('/top', (req, res) => {
+  // Get top story IDs
   rp('https://hacker-news.firebaseio.com/v0/topstories.json')
     .then((body) => {
+
+      // Only keep top 10 stories
       const stories = JSON.parse(body).slice(0, 10);
-      res.send(stories);
+
+      // Get story objects from stories array
+      getStories(stories)
+        .then((mappedStories) => (res.send(mappedStories)))
+        .catch((error) => (console.log(error)))
     })
     .catch((error) => (console.log(error)))
 });
+
+// Take array of story IDs and return array of story objects
+const getStories = stories => {
+  return Promise.all(
+    stories.map(
+      (story) => (rp('https://hacker-news.firebaseio.com/v0/item/' + story + '.json')
+        .then((data) => (JSON.parse(data)))
+      )
+    )
+  )
+}
 
 app.listen(8080);
 console.log("The server is now running on port 8080.");
