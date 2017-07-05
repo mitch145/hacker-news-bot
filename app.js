fcccf43cccaf7ea0bbdc45b6f3d517a407ba1693
Mitch Ball <mitchb145@gmail.com>
2017-07-05 13:53:02 +1000

    Add ping, cleanup /top and only send the first 10 stories

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ const request = require('request');
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/top', function(req, res){
+// Ping Server
+app.get('/', (req, res) => (res.send('Hello World')))
+
+// Get IDs of top 10 stories
+app.get('/top', (req, res) => {
   request('https://hacker-news.firebaseio.com/v0/topstories.json', (error, response, body) => {
     if (!error && response.statusCode == 200) {
-      res.send(JSON.parse(body));
+      // Get array of top 10 stories from body
+      const stories = JSON.parse(body).slice(0, 10);
+      console.log(stories)
+      res.send(stories);
     }
   })
 });
+
 app.listen(8080);
 console.log("The server is now running on port 8080.");
