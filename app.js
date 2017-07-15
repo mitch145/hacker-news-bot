ed190f694bb4a47a1afc1723fb5504ddca78cd13
Mitch Ball <mitchb145@gmail.com>
2017-07-15 13:14:23 +1000

    Update URLs again

--- app.js
+++ app.js
@@ -150,59 +150,99 @@ function sendGenericMessage(recipientId) {
 
 function sendListMessage(recipientId) {
   var messageData = {
-    recipient: {
-      id: recipientId
+    "recipient": {
+      "id": "RECIPIENT_ID"
     },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "list",
-          elements: [{
-              title: "Classic T-Shirt Collection",
-              image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-              subtitle: "See all our colors",
-              default_action: {
-                type: "web_url",
-                url: "https://www.oculus.com/en-us/rift/",
-                messenger_extensions: true,
-                webview_height_ratio: "tall",
-                fallback_url: "https://www.oculus.com/en-us/rift/"
+    "message": {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "list",
+          "elements": [{
+              "title": "Classic T-Shirt Collection",
+              "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+              "subtitle": "See all our colors",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/shop_collection",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
               },
-              buttons: [{
-                title: "View",
-                type: "web_url",
-                url: "https://www.oculus.com/en-us/rift/",
-                messenger_extensions: true,
-                webview_height_ratio: "tall",
-                fallback_url: "https://www.oculus.com/en-us/rift/"
+              "buttons": [{
+                "title": "View",
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
               }]
             },
             {
-              title: "Classic White T-Shirt",
-              image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-              subtitle: "100% Cotton, 200% Comfortable",
-              default_action: {
-                type: "web_url",
-                url: "https://www.oculus.com/en-us/rift/",
-                messenger_extensions: true,
-                webview_height_ratio: "tall",
-                fallback_url: "https://www.oculus.com/en-us/rift/"
+              "title": "Classic White T-Shirt",
+              "image_url": "https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png",
+              "subtitle": "100% Cotton, 200% Comfortable",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
               },
-              buttons: [{
-                title: "Shop Now",
-                type: "web_url",
-                url: "https://www.oculus.com/en-us/rift/",
-                messenger_extensions: true,
-                webview_height_ratio: "tall",
-                fallback_url: "https://www.oculus.com/en-us/rift/"
+              "buttons": [{
+                "title": "Shop Now",
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/shop?item=100",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
               }]
             },
+            {
+              "title": "Classic Blue T-Shirt",
+              "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+              "subtitle": "100% Cotton, 200% Comfortable",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+              },
+              "buttons": [{
+                "title": "Shop Now",
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+              }]
+            },
+            {
+              "title": "Classic Black T-Shirt",
+              "image_url": "https://peterssendreceiveapp.ngrok.io/img/black-t-shirt.png",
+              "subtitle": "100% Cotton, 200% Comfortable",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/view?item=102",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+              },
+              "buttons": [{
+                "title": "Shop Now",
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/shop?item=102",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+              }]
+            }
           ],
-          buttons: [{
-            title: "View More",
-            type: "postback",
-            payload: "payload"
+          "buttons": [{
+            "title": "View More",
+            "type": "postback",
+            "payload": "payload"
           }]
         }
       }
